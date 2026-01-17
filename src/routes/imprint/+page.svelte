afbc8ac92f397765ff1c1465323d81bfd2d0fb6b
Florian <127996345+florianblaschke@users.noreply.github.com>
2026-01-17 12:07:54 +0100

    add imprint and privacy page

---
+++ src/routes/imprint/+page.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+	import { buttonVariants } from "$lib/components/ui/button";
+	import { m } from "$lib/paraglide/messages";
+	import { cn } from "$lib/utils";
+</script>
+
+<section class="w-full grid items-center gap-8 lg:gap-12 min-h-[75vh]">
+	<h1 class="text-lg font-bold tracking-tighter sm:text-3xl md:text-5xl">
+		{m["imprint.title"]()}
+	</h1>
+	<article class="grid gap-4">
+		<h2 class="text-lg font-semibold">{m["imprint.company_name"]()}</h2>
+		<p class="text-medium font-normal text-gray-500">Bonani Drop in Centre</p>
+		<h2 class="text-lg font-semibold">{m["imprint.address"]()}</h2>
+		<p class="text-medium font-normal text-gray-500">
+			Kalinyanga P.O Box 108 Engcobo, 5050, South Africa
+		</p>
+		<h2 class="text-lg font-semibold">{m["imprint.represented"]()}</h2>
+		<p class="text-medium font-normal text-gray-500">Ayanda Mlahlwa</p>
+		<h2 class="text-lg font-semibold">{m["imprint.register.title"]()}</h2>
+		<p class=" text-gray-500">Social Development</p>
+		<div class="flex items-center gap-2 text-medium font-normal">
+			<p class="text-medium font-normal text-gray-500">287-015 NPO</p>
+		</div>
+	</article>
+	<article>
+		<h2 class="text-lg font-semibold">{m["imprint.image"]()}</h2>
+		<ul class="list-inside list-disc space-y-2">
+			<li class="text-medium font-normal">
+				<a
+					class={cn(
+						buttonVariants({
+							variant: "link",
+						}),
+						"px-0 text-base",
+					)}
+					target="_blank"
+					href="https://unsplash.com/de/@anniespratt?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+				>
+					Annie Spratt
+				</a>{" "}
+				on{" "}
+				<a
+					class={cn(
+						buttonVariants({
+							variant: "link",
+						}),
+						"px-0 text-base",
+					)}
+					target="_blank"
+					href="https://unsplash.com/de/fotos/gruppe-von-kinderfotografie-cVEOh_JJmEE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+				>
+					Unsplash
+				</a>
+			</li>
+			<li class="text-medium font-normal">
+				<a
+					class={cn(
+						buttonVariants({
+							variant: "link",
+						}),
+						"px-0 text-base",
+					)}
+					target="_blank"
+					href="https://unsplash.com/de/@ban_yido?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+				>
+					Ban Yido
+				</a>{" "}
+				on{" "}
+				<a
+					class={cn(
+						buttonVariants({
+							variant: "link",
+						}),
+						"px-0 text-base",
+					)}
+					target="_blank"
+					href="https://unsplash.com/de/fotos/silhouette-von-kindern-UmC0aBfur5c?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+				>
+					Unsplash
+				</a>
+			</li>
+		</ul>
+	</article>
+	<article class="space-y-2">
+		<p class="text-base font-semibold">
+			Homepage and Design by Florian Blaschke
+		</p>
+		<div class="flex flex-col md:flex-row gap-4 md:items-center">
+			<p class="flex gap-1 text-medium font-normal">
+				Visit:
+				<a
+					href="https://florianblaschke.me"
+					target="_blank"
+					class="hover:underline text-gray-500"
+				>
+					florianblaschke.me
+				</a>
+			</p>
+		</div>
+	</article>
+</section>
---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import { m } from "$lib/paraglide/messages";
+</script>
+
+<section class="grid items-center gap-8 lg:gap-12">
+  <h1 class="text-2xl font-bold tracking-tighter sm:text-3xl md:text-5xl">
+    {m["privacy.title"]()}
+  </h1>
+  <ul class="grid gap-8 list-inside list-decimal">
+    <li class="text-lg font-semibold">
+      <h2>{m["privacy.introduction.title"]()}</h2>
+      <p class="text-medium font-normal text-gray-500">
+        {m["privacy.introduction.text"]()}
+      </p>
+    </li>
+    <li class="text-lg font-semibold">
+      <h2>{m["privacy.data.title"]()}</h2>
+      <p class="text-medium font-normal text-gray-500">
+        {m["privacy.data.text"]()}
+      </p>
+    </li>
+    <li class="text-lg font-semibold">
+      <h2 class="pb-6">{m["privacy.language.title"]()}</h2>
+      <ul class="grid gap-6 list-disc list-inside pl-6">
+        <li class="text-lg font-semibold">
+          <h3>{m["privacy.language.bulletpoint_one_title"]()}</h3>
+          <p class="text-medium font-normal text-gray-500">
+            {m["privacy.language.bulletpoint_one"]()}
+          </p>
+        </li>
+        <li class="text-lg font-semibold">
+          <h3>{m["privacy.language.bulletpoint_two_title"]()}</h3>
+          <p class="text-medium font-normal text-gray-500">
+            {m["privacy.language.bulletpoint_two"]()}
+          </p>
+        </li>
+        <li class="text-lg font-semibold">
+          <h3>{m["privacy.language.bulletpoint_three_title"]()}</h3>
+          <p class="text-medium font-normal text-gray-500">
+            {m["privacy.language.bulletpoint_three"]()}
+          </p>
+        </li>
+      </ul>
+    </li>
+    <li class="text-lg font-semibold">
+      <h2>{m["privacy.security.title"]()}</h2>
+      <p class="text-medium font-normal text-gray-500">
+        {m["privacy.security.text"]()}
+      </p>
+    </li>
+    <li class="text-lg font-semibold">
+      <h2>{m["privacy.thirdParty.title"]()}</h2>
+      <p class="text-medium font-normal text-gray-500">
+        {m["privacy.thirdParty.text"]()}
+      </p>
+    </li>
+    <li class="text-lg font-semibold">
+      <h2>{m["privacy.rights.title"]()}</h2>
+      <p class="text-medium font-normal text-gray-500">
+        {m["privacy.rights.text"]()}
+      </p>
+    </li>
+    <li class="text-lg font-semibold">
+      <h2>{m["privacy.changes.title"]()}</h2>
+      <p class="text-medium font-normal text-gray-500">
+        {m["privacy.changes.text"]()}
+      </p>
+    </li>
+    <li class="text-lg font-semibold">
+      <h2>{m["privacy.contact.title"]()}</h2>
+      <p class="text-medium font-normal text-gray-500">
+        {m["privacy.contact.text"]()}
+      </p>
+    </li>
+  </ul>
+</section>
